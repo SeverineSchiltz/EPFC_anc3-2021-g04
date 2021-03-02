f468e80b0404312f4e9207c03084030e201fb73c
Ines Boudghene <ines.boudghene@gmail.com>
2021-03-02 20:51:15 +0100

    feat(MenuBar): add MenuBar in TrelloView

--- anc3_2021_projet_g04/src/main/java/view/TrelloView.java
+++ anc3_2021_projet_g04/src/main/java/view/TrelloView.java
@@ -1,6 +1,12 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import mvvm.*;
@@ -10,6 +16,7 @@ public class TrelloView extends VBox {
     private TrelloViewModel tvm;
     private EditableLabel boardTitle;
     private BoardView boardView;
+    public MenuBar menuBar;
 
 
     public TrelloView(Stage primaryStage, TrelloViewModel viewModel){
@@ -24,13 +31,59 @@ public class TrelloView extends VBox {
     private void config(){
         this.boardTitle.setId("boardTitle");
         this.boardView.setId("board");
+        // TODO: to check if necessary
+        this.menuBar.setId("menuBar");
         this.getStylesheets().add("/trello.css");
     }
 
     private void addComponent(){
+        // create a MenuBar
+        this.menuBar = new MenuBar();
+        // create Menus (to display on the MenuBar)
+        Menu mnFile = new Menu("Fichier");
+        Menu mnEdition = new Menu("Edition");
+        // create MenuItems (to display on the Menus)
+        MenuItem mniNewColumn = new MenuItem("Nouvelle colonne");
+        MenuItem mniQuit = new MenuItem("Quitter");
+        MenuItem mniUndo = new MenuItem("Annuler");
+        MenuItem mniRedo = new MenuItem("Refaire");
+        // add MenuItems to the Menus
+        mnFile.getItems().addAll(mniNewColumn, mniQuit);
+        mnEdition.getItems().addAll(mniUndo, mniRedo);
+//        // TODO: only to check so likely to remove
+//        mniUndo.setDisable(true);
+//        mniRedo.setDisable(true);
+        // add Menus to the MenuBar
+        menuBar.getMenus().addAll(mnFile, mnEdition);
+
+        // TODO: this is not the right place
+        // when user enters "ctrl+x" to close which is the accelerator for "Quitter" MenuItem
+        mniQuit.setAccelerator(KeyCombination.keyCombination("Ctrl+X"));
+        // when user enters "ctrl+z" to undo which is the accelerator for "Annuler" MenuItem
+        mniUndo.setAccelerator(KeyCombination.keyCombination("Ctrl+Z"));
+        // when user enters "ctrl+y" to redo which is the accelerator for "Refaire" MenuItem
+        mniRedo.setAccelerator(KeyCombination.keyCombination("Ctrl+Y"));
+
+        // when user clicks on "Quitter" MenuItem
+        mniQuit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
+        // when user clicks on "Nouvelle colonne" MenuItem
+        mniNewColumn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO: this does not respect the Demeter law
+                tvm.getBoardVM().addColumn();
+            }
+        });
+
         boardTitle = new EditableLabel(tvm.getBoardTitleProperty());
         boardView = new BoardView(tvm.getBoardVM());
-        this.getChildren().addAll(boardTitle, boardView);
+        this.getChildren().addAll(menuBar, boardTitle, boardView);
     }
 
     private void configBindings(Stage primaryStage) {
